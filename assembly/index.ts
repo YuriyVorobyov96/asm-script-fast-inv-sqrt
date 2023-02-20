1ad6281cd9ee69a0cacac7dc6903c3d9235a324a
YuriyVorobyov96 <darthrevan1333@gmail.com>
2023-02-20 19:13:41 +0300

    switched to built-in reinterpret function

--- assembly/index.ts
+++ assembly/index.ts
@@ -2,11 +2,11 @@ export function fastInvSqrt(number: f32): f32 {
   const halfNumber: f32 = number * 0.5;
   const threeHalfs: f32 = 1.5;
 
-  let i = Int32Array.wrap(new Float32Array(1).fill(number).buffer)[0];
+  let i = reinterpret<u32>(number);
 
   i = 0x5f3759df - (i >> 1); // what the fuck? :)
 
-  number = Float32Array.wrap(new Int32Array(1).fill(i).buffer)[0];
+  number = reinterpret<f32>(i);
 
   number = number * (threeHalfs - (halfNumber * number * number));
 
